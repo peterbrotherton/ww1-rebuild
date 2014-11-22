c02c55bb8258167e09f5f3cceb49804c3870cad9
peterbrotherton <peter.brotherton@sl.nsw.gov.au>
2014-11-22 11:32:57 +1100

    timeline

--- slnswww1/js/slnswww1.behaviors.js
+++ slnswww1/js/slnswww1.behaviors.js
@@ -89,38 +89,67 @@
 			});
 		}
 		
-		function initPageTimeline() {
-			if (!$('body.page-explore-timeline-new').length) { return; }
-			
-			$('#timeline-nav a').click(function(event){
-				var startDate = $(this).parent().data('date');
-				var endDate = $(this).parent().next().data('date');
-				$('#edit-field-date-references-value-1-value-date').val(startDate);
-				$('#edit-field-date-references-value-2-value-date').val(endDate);
-				
-				$.ajax({
-					url: '/views/ajax',
-					type: 'post',
-					data: {
-						view_name: 'asset_list_timeline',
-						view_display_id: 'panel_pane_1',
-						'field_date_references_value_1[value][date]': startDate,
-						'field_date_references_value_2[value][date]': endDate
-					},
-					dataType: 'json',
-					success: function (response) {
-						if (response[1] !== undefined) {
-							var viewHtml = $(response[1].data);
-							var itemList = viewHtml.find('.view-content .item-list');
-							$('.view-asset-list-timeline .view-content .item-list').fadeOut("slow", function() {
-								$('.view-asset-list-timeline .view-content').html(itemList);
-							});
-							
-							//$('.view-asset-list-timeline .view-content').animate({ "top": "-=500px" }, "slow" );
-						}
+		function refreshTimeline(startDate, endDate) {
+			$.ajax({
+				url: '/views/ajax',
+				type: 'post',
+				data: {
+					view_name: 'asset_list_timeline',
+					view_display_id: 'panel_pane_1',
+					'field_date_references_value_1[value][date]': startDate,
+					'field_date_references_value_2[value][date]': endDate
+				},
+				dataType: 'json',
+				success: function (response) {
+					if (response[1] !== undefined) {
+						var viewHtml = $(response[1].data);
+						var itemList = viewHtml.find('.view-content .item-list');
+						$('.view-asset-list-timeline .view-content .item-list').fadeOut("slow", function() {
+							var year = startDate.substring(3); 
+							$('.view-asset-list-timeline .view-content').html('<h2 class="year">' + year + '</h2>');
+							if (itemList.length) {
+								$('.view-asset-list-timeline .view-content').append(itemList);
+							} else {
+								$('.view-asset-list-timeline .view-content').append('<div class="item-list"><h2>No results for selected date, please choose again.</h2></div>');
+							}
+						});
 					}
-				});
+				}
+			});
+		}
+		
+		function initPageTimeline() {
+			if (!$('body.page-explore-timeline').length) { return; }
+			
+			// hide timeline links, match initial view state
+			$('#timeline-nav ul').css('display', 'none');
+			$('#timeline-nav ul:first').css('display', 'block');
+			$('#timeline-nav li:first a').addClass('active');
+			$('.view-asset-list-timeline .view-content').prepend('<h2 class="year">1914</h2>');
+			
+			// month clicked
+			$('#timeline-nav li ul li a').click(function(event) {
+				$('#timeline-nav a').removeClass('active');
+				$(this).addClass('active');
 				
+				var startDate = $(this).parent().data('date');
+				var endDate = startDate;
+				refreshTimeline(startDate, endDate);
+				event.preventDefault();
+			});
+			
+			// year clicked
+			$('#timeline-nav > li > a').click(function(event) {
+				$(this).parent().siblings().children('ul').css('display', 'none');
+				$(this).parent().find('ul').css('display', 'block');
+				
+				$('#timeline-nav a').removeClass('active');
+				$(this).addClass('active');
+				$(this).parent().find('ul a').addClass('active');
+				
+				var startDate = $(this).parent().find('ul li').first().data('date');
+				var endDate = $(this).parent().find('ul li').last().data('date');
+				refreshTimeline(startDate, endDate);
 				event.preventDefault();
 			});
 		}
